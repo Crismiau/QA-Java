e6b22da95dbe60d3a8395f784e498d293fb3dbc3
Crismiau <heycristian26@gmail.com>
2025-09-24 13:53:38 -0500

    Update Comments

--- src/main/java/com/example/qa/UserService.java
+++ src/main/java/com/example/qa/UserService.java
@@ -2,13 +2,23 @@ package com.example.qa;
 
 public class UserService {
     private final UserRepository repo;
+    // aqui yo hago referencia al interfaz y lo guardo en una variable
 
     public UserService(UserRepository repo) {
         this.repo = repo;
-    }
+    } // aqui creo el constructor para copiarlo o simularlo en pro de mockito
 
     public boolean register(String email) {
-        if (repo.existsByEmail(email)) return false;
+        if (repo.existsByEmail(email))
+            return false;
+
+            /*
+                La lógica del metodo register:
+                llamo el interfaz y a su vez con el . me traigo el metodo para verificar la existencia del correo
+                Si ese metodo devuelve true → significa que el correo YA EXISTE → entonces register devuelve false (no se puede registrar).
+                Si ese metodo devuelve false → significa que el correo NO EXISTE → entonces register devuelve true (se puede registrar).
+
+             */
         return true;
     }
 }
--- src/test/java/com/example/qa/UserServiceTest.java
+++ src/test/java/com/example/qa/UserServiceTest.java
@@ -4,15 +4,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class UserServiceTest {
-    @Test
-    void register_existingEmail_returnsFalse() {
-        UserRepository repo = mock(UserRepository.class);
-        when(repo.existsByEmail("a@a.com")).thenReturn(true);
+// Mockito nos ayuda a simular o copiar una (fuente de datos, conexión de API, etc.) y con esa copia poder interactuar libremente
+// para hacer pruebas unitarias.
+// Esto es generalmente util cuando nuestra clase depende de una fuente de datos, alguna API O muy específico el caso de que si nuestra clase
+// tiene comportamientos o funciones muy difíciles de reproducir..
 
-        UserService service = new UserService(repo);
+    class UserServiceTest {
 
-        assertFalse(service.register("a@a.com"));
-        verify(repo).existsByEmail("a@a.com");
+        // Aca especificamos a Junit que este metodo sera un codigo tipo test, para que lo pueda ejecutar como TEST y no como codigo normal
+        @Test
+        void register_existingEmail_returnsFalse() {
+
+            // 1. Aqui creamos un mock (simulación o  copia) del repositorio de users (En un sistema real seria como una base de datos)
+            UserRepository repo = mock(UserRepository.class);
+
+            // 2. Configurar comportamiento del mock
+            when(repo.existsByEmail("a@a.com")).thenReturn(true);
+
+                    // con el when especifico una especie de condición:
+                    // cuando se acceda al metodo del mock (repo) responde con una respuesta predeterminada:
+                    // en esta caso cuando se acceda a la validacion del correo que retorne un true
+
+                    // la logica aqui es que el correo que hay en el parametro yo lo voy a definir como que ya existe en el mock
+                    // por ende entonces será false para el metodo de register, ya que el correo YA EXISTE.
+
+
+            // 3. Luego probamos la clase con el mock
+            UserService service = new UserService(repo); // instancio la clase userService para traerme el metodo de register.
+            assertFalse(service.register("a@a.com"));
+           verify(repo).existsByEmail("a@a.com");
+
+           // tarea: hacer el sout para mostrar en consola que muestra verify y assertfalse.
+
+
+            // Un caso de uso de este código es que si queremos conocer que usuarios ya estan registrados en nuestra página
+            // devuelvo true si el usuario esta registrado y devolviendo false si no,
+        }
     }
-}
